4f5ffe864b1e9f4c5a267e7b01d514f6cf65a185
danshkk <shkurkodan@gmail.com>
2021-05-11 22:56:37 +0300

    finale

--- script.js
+++ script.js
@@ -15,4 +15,28 @@ new Swiper('.feedback-slider', {
         disableOnIteraction: false
     },
     speed: 500
-}); 
+});
+
+$(document).ready(function() {
+	$('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
